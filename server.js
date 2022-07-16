9c8610537d15f1d3274473fb09fb65687d614688
DanSasi <75125182+DanSasi@users.noreply.github.com>
2022-07-16 13:46:18 +0300

    Testing the server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello To Our Great Team!');
+    res.send('Testing!');
 });
 
 app.listen(PORT, HOST);
