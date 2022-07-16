9b21c6adfcdcaf928e8c1af2f7c480a4af78e808
DanSasi <75125182+DanSasi@users.noreply.github.com>
2022-07-16 14:23:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-var filePath = 'D:\devops_photo.jpg';
+var song = "Is this tree an AVL? Or a binary tree? Caught in a while loop... No escape from the boolean true. Open your eyes and just insert a break, break free!!!! Im just an udergrad, give me some sympathy... because Im open for changes and closed for expansion. Anyway you teach me dosent really matter to me, to me";
 app.get('/', (req, res) => {
-    res.sendFile(filePath);
+    res.send(song);
 });
 
 app.listen(PORT, HOST);
