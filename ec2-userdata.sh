5d618426b725ce3657a7f5e48ede589e1a43cf76
DanSasi <75125182+DanSasi@users.noreply.github.com>
2022-07-17 10:00:56 +0300

    Update ec2-userdata.sh

--- ec2-userdata.sh
+++ ec2-userdata.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+
+# Installing Docker Engine
+
 curl https://get.docker.com | sh -
+
+# Installing Docker-Compose
 sudo apt install docker-compose -y
+
+#Adding ubuntu to docker group for permissions
+usermod -aG docker ubuntu
+
+# Adding Compose Configuration File to point to our project
 
 echo """version: '3.3'
 services:
@@ -11,5 +21,10 @@ services:
         image: 'ds94/devops-proj-01:latest'""" > /opt/docker-compose.yml
 
 
+# First Start of the project
 docker-compose -f /opt/docker-compose.yml up -d
-echo "* * * * * root docker-compose -f /opt/docker-compose.yml pull && docker-compose -f /opt/docker-compose.yml up -d" > /etc/cron.d/dockerjobs
+
+#github action ssh with the command  "docker-compose -f /opt/docker-compose.yml pull && docker-compose -f /opt/docker-compose.yml up -d" for deployment
+
+# If we want auto-deploy on listening mode then we would use:
+#echo "* * * * * root docker-compose -f /opt/docker-compose.yml pull && docker-compose -f /opt/docker-compose.yml up -d" > /etc/cron.d/dockerjobs
