7b6b694098ea81083ce794ee8d8826edd09fbe84
Michael <72190719+MichaelBR95@users.noreply.github.com>
2022-07-21 16:09:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-var song = "Is this tree an AVL? Or a binary tree? Caught in a while loop... No escape from the boolean true. Open your eyes and just insert a break, break free!!!! Im just an udergrad, give me some sympathy... because Im open for changes and closed for expansion. Anyway you teach me dosent really matter to me, to me";
+var song = "Is this tree an AVL? Or a binary tree? Caught in a while loop... No escape from the boolean true. Open your eyes and just insert a break, break free! Im just an udergrad, give me some sympathy... because Im open for changes and closed for expansion. Anyway you teach me dosent really matter to me, to me";
 app.get('/', (req, res) => {
     res.send(song);
 });
