2781e9840bb5aa6a604d951f0c09d7a8a0411c51
Michael <72190719+MichaelBR95@users.noreply.github.com>
2022-07-21 16:17:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello To Our Great Team!');
+    res.send('Hello To Our Great Team!!!');
 });
 
 app.listen(PORT, HOST);
